4c2cce6b91051091a790bceb9c293955cfd9e7a2
Meifanny Azri Shafira <meifannyshafira@gmail.com>
2021-10-18 13:04:18 +0700

    first commit

---
+++ JavaBasic1.java
@@ -0,0 +1,13 @@
+public class JavaBasic1 {
+    public static void Syaratmasuksekolahdasar (int umur){
+        if (umur < 7) {
+            System.out.println("Anda Tidak Layak Masuk");   
+          } else {
+            System.out.println("Anda Layak Masuk"); 
+          }
+          
+        } 
+        public static void main(String[] args) { 
+          Syaratmasuksekolahdasar(4); 
+        }
+    }
